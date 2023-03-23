6191025277256bd1f32cbc7eb598711cb4871e86
carloscasellas <carloscg10@yahoo.com>
2023-03-23 18:16:41 +0000

    Finished quiz2-2

--- printvars.c
+++ printvars.c
@@ -8,6 +8,36 @@ char *custom_env[] = {"USER=Pikachu", "PATH=/tmp", NULL};
 
 int main(void)
 {
-    // TODO
+    
+    int waitid;
+
+    pid_t pid = fork();
+
+    if(pid != 0) {
+
+        printf("Running parent process PID: %d\n\n", getpid());
+
+    } else {
+
+        printf("Running child process PID: %d\n", getpid());
+        execle((char*) "echoall", (char*) "echoall" , (char*) "Bandicoot", (char*) "Pacman", NULL, custom_env);
+        exit(0);
+
+    }
+
+    waitpid(pid, &waitid, 0);
+
+    pid_t otherPid = fork();
+    if(otherPid == 0) {
+
+        printf("Running child process PID: %d\n", getpid());
+        execlp((char*) "echoall", (char*) "echoall" , (char*) "Spyro", NULL);
+        exit(0);
+
+    }
+
+    waitpid(otherPid, &waitid, 0);
+
     exit(0);
+
 }
